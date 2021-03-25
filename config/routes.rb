03abfdf43e0c34d0122371d5d1383bf4f7ea6362
sandeep182 <s.sandeep@kloctechnologies.co.in>
2021-03-25 12:51:27 +0530

    feat: added delete multiple recored using checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :product_images do
     collection { post :import }
+    delete 'delete_multiple', on: :collection
   end
   root 'product_images#index'
   get '/all_products', to: 'product_images#all_products', as: 'all_products'
